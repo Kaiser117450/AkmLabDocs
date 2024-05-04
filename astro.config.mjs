abd8136fd494403059ea21b35c403c68281a0ecb
Kaiser117450 <118813333+Kaiser117450@users.noreply.github.com>
2024-05-04 02:56:25 +0000

    adding censor on NPWP

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://Kaiser117450.github.io',
+	base: 'AkmLabDocs',
 	integrations: [
 		starlight({
 			favicon: 'images/favicon.png',
